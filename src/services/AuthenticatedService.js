f5bec1d114c5552a154d98053285501a009f79a0
Renato De la Rosa <rdelarosa@sodimac.com.pe>
2021-07-14 19:25:00 -0500

    Axios Services

--- src/services/AuthenticatedService.js
+++ src/services/AuthenticatedService.js
@@ -1,16 +1,11 @@
+import axios from 'axios';
 import md5 from 'md5';
 
 const API_URL = 'http://localhost:3027/users';
-const options = {
-  headers: {
-    'Content-type': 'application/json',
-    Accept: 'application/json',
-  },
-};
 
 const SignIn = async (userToLogin) => {
-  const response = await fetch(API_URL, options);
-  const users = await response.json();
+  const responseUsers = await axios.get(API_URL);
+  const users = responseUsers.data;
   let result = {};
 
   let userFilter = users.filter(
@@ -39,8 +34,8 @@ const SignIn = async (userToLogin) => {
 
 const SignUp = async (userToRegister) => {
   let result = {};
-  const responseGet = await fetch(API_URL, options);
-  const users = await responseGet.json();
+  const responseUsers = await axios.get(API_URL);
+  const users = responseUsers.data;
   const usersFilter = users.filter(
     (user) => user.email === userToRegister.email
   );
@@ -51,18 +46,11 @@ const SignUp = async (userToRegister) => {
       userExists: true,
     };
   } else {
-    let optionsCreateUser = options;
-    optionsCreateUser = {
-      ...optionsCreateUser,
-      method: 'POST',
-      body: JSON.stringify({
-        ...userToRegister,
-        password: btoa(userToRegister.password),
-      }),
-    };
-
-    const responsePost = await fetch(API_URL, optionsCreateUser);
-    const dataUser = await responsePost.json();
+    const responseCreate = await axios.post(API_URL, {
+      ...userToRegister,
+      password: btoa(userToRegister.password),
+    });
+    const dataUser = responseCreate.data;
 
     result = {
       error: false,
@@ -76,18 +64,15 @@ const SignUp = async (userToRegister) => {
 };
 
 const GetUserById = async (id) => {
-  const responseGet = await fetch(`${API_URL}/${id}`, options);
-  const userData = await responseGet.json();
+  const responseGet = await axios.get(`${API_URL}/${id}`);
+  const userData = responseGet.data;
   userData.photoUrl = `https://www.gravatar.com/avatar/${md5(userData.email)}`;
   return userData;
 };
 
 const UpdateUser = async (id, userToUpdate) => {
-  const optionsUpdate = options;
-  optionsUpdate.method = 'PUT';
-  optionsUpdate.body = JSON.stringify(userToUpdate);
-  const responseUpdate = await fetch(`${API_URL}/${id}`, optionsUpdate);
-  const userUpdate = await responseUpdate.json();
+  const responseUpdate = await axios.put(`${API_URL}/${id}`, userToUpdate);
+  const userUpdate = responseUpdate.data;
 
   return userUpdate;
 };
--- src/services/RickAndMortyService.js
+++ src/services/RickAndMortyService.js
@@ -1,14 +1,10 @@
+import axios from 'axios';
+
 const API_URL = 'https://rickandmortyapi.com/api/character';
-const options = {
-  headers: {
-    'Content-type': 'application/json',
-    Accept: 'application/json',
-  },
-};
 
 const getCharactersRickAndMorty = async (url = API_URL) => {
-  const responseCharacters = await fetch(url, options);
-  const dataCharacters = await responseCharacters.json();
+  const responseCharacters = await axios.get(url);
+  const dataCharacters = responseCharacters.data;
   const characters = dataCharacters.results;
   const next = dataCharacters.info.next;
   const prev = dataCharacters.info.prev;
